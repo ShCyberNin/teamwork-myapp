106f2549269acab2c22c7d5f8df27167f438f162
ShCyberNin <md.shahril1991@gmail.com>
2024-05-26 10:34:33 +0800

    new files uploaded

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import Provider from "@/components/Provider";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TeamWork",
+  description: "Collaborate. Create. Succeed",
+  icons: [
+    {
+      url: "/logo.svg",
+      href: "/logo.svg",
+    }
+  ]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider>
+          <main>
+          {children}
+          </main>
+        <Toaster />
+        </Provider>
+      </body>
+    </html>
+  );
+}
